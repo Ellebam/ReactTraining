d44a994e6e400d791d4c122c68e4fd3d20b4dad7
Ellebam <arian.jamborzadeh@gmail.com>
2020-10-25 20:05:03 +0100

    new folder for states

---
+++ 03_seasons/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: 1000000000000000000000 };
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        //we called setState!!!!
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => console.log(err)
+    );
+  }
+
+  //React says we have to define render!!
+  render() {
+    return <div>Latitude: {this.state.lat}</div>;
+  }
+}
+ReactDOM.render(<App />, document.querySelector('#root'));
