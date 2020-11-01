34eacfd79489272079f2085716d802cf05a544c7
Ellebam <arian.jamborzadeh@gmail.com>
2020-11-01 19:37:12 +0100

    finished dropdown

--- 06_widgets/src/App.js
+++ 06_widgets/src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion';
+import Dropdown from './components/Dropdown';
 import Search from './components/Search';
 
 const items = [
@@ -18,10 +19,31 @@ const items = [
   },
 ];
 
+const options = [
+  {
+    label: 'The color Red',
+    value: 'red',
+  },
+  {
+    label: 'The color Green',
+    value: 'green',
+  },
+  {
+    label: 'A shade of blue',
+    value: 'blue',
+  },
+];
+
 export default () => {
+  const [selected, setSelected] = useState(options[0]);
+
   return (
     <div>
-      <Search />
+      <Dropdown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+      />
     </div>
   );
 };
---
+++ 06_widgets/src/components/Dropdown.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect, useRef } from 'react';
+import './Dropdown.css';
+
+const Dropdown = ({ options, selected, onSelectedChange }) => {
+  const [open, setOpen] = useState(false);
+
+  const ref = useRef();
+
+  useEffect(() => {
+    document.body.addEventListener(
+      'click',
+      (event) => {
+        if (ref.current && ref.current.contains(event.target)) {
+          return;
+        }
+        setOpen(false);
+      },
+      { capture: true }
+    );
+  }, []);
+
+  const renderedOptions = options.map((option) => {
+    if (option.value === selected.value) {
+      return null;
+    }
+    return (
+      <div
+        key={option.value}
+        className='item'
+        onClick={() => {
+          onSelectedChange(option);
+
+          console.log(selected.value);
+        }}
+      >
+        {' '}
+        {option.label}
+      </div>
+    );
+  });
+
+  return (
+    <div ref={ref} className='ui form'>
+      <div className='field'>
+        <label className='label'>Select a Color</label>
+        <div
+          onClick={() => setOpen(!open)}
+          className={`ui selection dropdown ${open ? 'visible active' : ''} `}
+        >
+          <i className='dropdown icon'></i>
+          <div className='text'>{selected.label}</div>
+          <div className={`menu ${open ? 'visible transition' : ''} `}>
+            {renderedOptions}
+          </div>
+        </div>
+        <div className='ui container segment raised'>
+          <p className={`special-thing ${selected.value}`}>
+            {' '}
+            This text changes color
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dropdown;
