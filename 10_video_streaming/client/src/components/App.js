a12015252517f82f6d7a39d1d97770c867ff0426
Ellebam <arian.jamborzadeh@gmail.com>
2020-11-06 17:42:38 +0100

    created new Stream Project

---
+++ 10_video_streaming/client/src/components/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import Header from './Header';
+
+const App = () => {
+  return (
+    <div classsName='ui container'>
+      <BrowserRouter>
+        <Header />
+        <div className='ui container'>
+          <Route path='/' exact component={StreamList} />
+          <Route path='/new' exact component={StreamCreate} />
+          <Route path='/edit' exact component={StreamEdit} />
+          <Route path='/delete' exact component={StreamDelete} />
+          <Route path='/show' exact component={StreamShow} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
